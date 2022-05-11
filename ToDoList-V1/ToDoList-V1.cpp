dbf37f5fee22ae2b89c2dbc23621472137414a27
Archilson1988 <avalyan.ervand@gmail.com>
2022-05-11 22:42:10 +0300

    added task

--- ToDoList-V1/ToDoList-V1.cpp
+++ ToDoList-V1/ToDoList-V1.cpp
@@ -1,9 +1,36 @@
-
-
-#include <iostream>
+#include "Header.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "");
+
+
+	return 0;
 }
 
+/*Практическая работа
+
+1. Добавить
+
+2. Удалить
+
+3. Изменить
+
+4. Очистить весь список дел
+
+Структура TodoItem : id, name, description, due_date, что - то еще...
+
+Структура TodoList : id, name, todoItems[]
+
+GIT:
+
+Создать ветку develop
+
+Для каждой отдельной функции реализовать отдельную ветку features / номер - задачи - название - задачи
+после имплементации в ветках - merge в develop веток
+
+Решить возможные merge конфликты
+
+Локальные feature ветки удалить, в origin(github) эти ветки оставить
+
+Слить все в master(develop оставить локально и удаленно) */
